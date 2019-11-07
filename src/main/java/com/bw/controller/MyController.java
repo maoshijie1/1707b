f40f7482e90c0168f8b37a95fab716eb867987c3
msj <maoshijie@qq.com>
2019-11-07 09:48:03 +0800

    第一次提交

---
+++ src/main/java/com/bw/controller/MyController.java
@@ -0,0 +1,12 @@
+package com.bw.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MyController {
+	@RequestMapping("/list")
+	public String list(){
+		return "list";
+	}
+}
